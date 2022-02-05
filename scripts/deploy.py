95c409408cca4b26e64d3a9426b99fc8e43ebf16
Vitor Frasson <vitorfrasson@Vitors-MacBook-Pro.local>
2022-02-05 16:50:56 +1000

    compiling successfully

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -22,13 +22,15 @@ def deploy():
         {"from": account},
         # publish_source=True,
     )
-    app_tx = approve_erc20(10, charity_casino.address, matic_token, account)
-    transferFrom(account, charity_casino.address, 10, matic_token, charity_casino)
-
+    bet = 10
     link_tx = fund_with_link(charity_casino.address)
     link_tx.wait(1)
-    matic_tx = fund_with_matic(charity_casino.address)
-    matic_tx.wait(1)
+    add_token = charity_casino.addAllowedTokens(link_token, {"from": account})
+    app_tx = approve_erc20(bet, charity_casino.address, link_token, account)
+    transferFrom(account, charity_casino.address, bet, link_token, charity_casino)
+
+    # matic_tx = fund_with_matic(charity_casino.address)
+    # matic_tx.wait(1)
 
 
 def transferFrom(_from, _to, _amount, _erc20_address, _account):
@@ -43,15 +45,6 @@ def approve_erc20(_amount, _spender, _erc20_address, _account):
     tx = erc20.approve(_spender, _amount, {"from": _account})
     tx.wait(1)
     print("ERC20 Approved")
-
-
-def erc20_allowance(_spender, _erc20_address, _account, _owner):
-    erc20 = interface.IERC20(_erc20_address)
-    tx = erc20.allowance(_owner, _spender, {"from": _account})
-    tx.wait(1)
-    print(
-        "ERC20 Allowance is",
-    )
 
 
 def main():
